c8a5b4209a5e01ad17d80186957c0c50e65c9951
ginphamnois <chinh.pham@nois.vn>
2024-10-14 11:38:10 +0700

    feat: handle message from reactnative webview

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // const [code, setCode ] = useState('');
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    // Function to handle messages from React Native
+    const handleMessage = (event: any) => {
+      const data = event.data;
+
+      // Only handle messages with expected data format
+      if (data && data.type === "data") {
+        setMessage(data.payload); // Update state with the received data
+      }
+    };
+
+    // Add the message listener when the component mounts
+    window.addEventListener("message", handleMessage);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>{message}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
